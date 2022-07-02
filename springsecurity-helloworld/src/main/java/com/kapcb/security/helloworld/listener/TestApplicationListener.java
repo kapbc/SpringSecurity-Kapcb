1fd275197bfa491a9ec0ed2fd72863fcf0d6a8cc
cb <chenb16@mingyuanyun.com>
2022-07-02 12:29:27 +0800

    Add Spring Application Listener Code

--- springsecurity-helloworld/src/main/java/com/kapcb/security/helloworld/listener/TestApplicationListener.java
+++ springsecurity-helloworld/src/main/java/com/kapcb/security/helloworld/listener/TestApplicationListener.java
@@ -3,6 +3,7 @@ package com.kapcb.security.helloworld.listener;
 import com.kapcb.security.helloworld.event.TestApplicationEvent;
 import com.kapcb.security.helloworld.pojo.bo.EventBO;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
 /**
  * <a>Title: TestApplicationListener </a>
@@ -14,13 +15,13 @@ import org.springframework.context.ApplicationListener;
  * @date 2022/7/2 12:22
  * @since 1.0
  */
+@Component
 public class TestApplicationListener implements ApplicationListener<TestApplicationEvent> {
 
     @Override
     public void onApplicationEvent(TestApplicationEvent event) {
         EventBO source = (EventBO) event.getSource();
-
-
+        System.out.println("Hello " + source.getName());
     }
 
 }
