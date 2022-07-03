b91058ccf3ab2810df2a4062923d822264b1c586
cb <chenb16@mingyuanyun.com>
2022-07-03 18:35:40 +0800

    add SecurityContextHolder

---
+++ springsecurity-helloworld/src/main/java/com/kapcb/security/helloworld/controller/UserController.java
@@ -0,0 +1,37 @@
+package com.kapcb.security.helloworld.controller;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * <a>Title: UserController </a>
+ * <a>Author: cb <a>
+ * <a>Description: UserController <a>
+ *
+ * @author cb
+ * @version 1.0
+ * @date 2022/7/3 15:31
+ * @since 1.0
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+    @GetMapping("user-info")
+    public String userInfo() {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        String name = authentication.getName();
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        Object credentials = authentication.getCredentials();
+        Object principal = authentication.getPrincipal();
+        System.out.println("name : " + name + "'s credentials is : " + credentials + " principal is : " + principal + " authorities is : " + authorities);
+        return name;
+    }
+
+}
